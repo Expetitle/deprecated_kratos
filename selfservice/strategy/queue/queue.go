a0b9fb243232f026718e2703db50f35622afeeda
Kostya Kostyushko <kostanos@gmail.com>
2021-04-28 14:20:27 +0300

    EXP-1369 Using Rabbitmq Exchange instead of Queue

--- selfservice/strategy/queue/queue.go
+++ selfservice/strategy/queue/queue.go
@@ -38,9 +38,9 @@ type (
 	}
 )
 
-func SendVerificationQueue(identity *identity.Identity, address *identity.VerifiableAddress, rabbitMQURL string, verifyURL string, l *logrusx.Logger) {
-	var firstName = gjson.GetBytes(identity.Traits, "name.first").String()
-	var lastName = gjson.GetBytes(identity.Traits, "name.last").String()
+func SendMessageToQueue(rabbitMQURL string, routing string, message string, l *logrusx.Logger) {
+	// TODO: add better logging for which message, to where, was sent
+	// TODO: the rabbitMQURL should not be passed as parameter, but taked from ENV directly from here
 	conn, err := amqp.Dial(rabbitMQURL)
 	if err != nil {
 		l.Warn("Failed to connect to RabbitMQ")
@@ -52,78 +52,54 @@ func SendVerificationQueue(identity *identity.Identity, address *identity.Verifi
 			defer ch.Close()
 		} else {
 			l.Info("connected to RabbitMQ")
-			q, err := ch.QueueDeclare("MAIL_NEW_ACCOUNT", false, false, false, false, nil)
-			l.Info(q)
+			// TODO: the exchange name should be in ENV variables/config
+			exc, err := ch.ExchangeDeclare("users_exchange", "fanout" true, false, false, false, nil)
+			l.Info(exc)
 			// Handle any errors if we were unable to create the queue
 			if err != nil {
-				l.Warn("Failed to create a queue with name MAIL_NEW_ACCOUNT")
+				l.Warn("Failed to declare an exchange with name users_exchange")
 			} else {
-				emailUserData := EmailUserData{Id: identity.ID.String(), Email: address.Value, FirstName: firstName,
-					LastName: lastName, Name: firstName + " " + lastName, ConfirmSignup: verifyURL}
-				emailUser := EmailUser{User: emailUserData}
-				data := NewAccountEmailData{EventType: "NewAccount", Data: emailUser}
-				stringifyData, _ := json.Marshal(data)
-				l.Info(string(stringifyData))
 				err = ch.Publish(
-					"",
-					"MAIL_NEW_ACCOUNT",
+					"users_exchange",
+					routing,
 					false,
 					false,
 					amqp.Publishing{
 						ContentType: "text/plain",
-						Body:        []byte(string(stringifyData)),
+						Body:        []byte(message),
 					},
 				)
 
 				if err != nil {
 					l.Warn(err)
 				}
-				l.Info("Successfully Published Message to Queue")
+				l.Info("Successfully Published Message to exchange")
 			}
 		}
 	}
 }
 
+func SendVerificationQueue(identity *identity.Identity, address *identity.VerifiableAddress, rabbitMQURL string, verifyURL string, l *logrusx.Logger) {
+	var firstName = gjson.GetBytes(identity.Traits, "name.first").String()
+	var lastName = gjson.GetBytes(identity.Traits, "name.last").String()
+	emailUserData := EmailUserData{Id: identity.ID.String(), Email: address.Value, FirstName: firstName,
+		LastName: lastName, Name: firstName + " " + lastName, ConfirmSignup: verifyURL}
+	emailUser := EmailUser{User: emailUserData}
+	data := NewAccountEmailData{EventType: "NewAccount", Data: emailUser}
+	stringifyData, _ := json.Marshal(data)
+	l.Info(string(stringifyData))
+	// TODO: the routing name should be default and in ENV variables/config
+	SendMessageToQueue(rabbitMQURL, "MAIL_NEW_ACCOUNT", string(stringifyData), l)
+}
+
 func SendRecoveryQueue(identity *identity.Identity, address *identity.RecoveryAddress, rabbitMQURL string, recoveryURL string, l *logrusx.Logger) {
 	var firstName = gjson.GetBytes(identity.Traits, "name.first").String()
 	var lastName = gjson.GetBytes(identity.Traits, "name.last").String()
-	conn, err := amqp.Dial(rabbitMQURL)
-	if err != nil {
-		l.Warn("Failed to connect to RabbitMQ")
-		defer conn.Close()
-	} else {
-		ch, err := conn.Channel()
-		if err != nil {
-			l.Warn("Failed to open a channel")
-			defer ch.Close()
-		} else {
-			l.Info("connected to RabbitMQ")
-			q, err := ch.QueueDeclare("RESET_PASSWORD_REQUEST", false, false, false, false, nil)
-			l.Info(q)
-			// Handle any errors if we were unable to create the queue
-			if err != nil {
-				l.Warn("Failed to create a queue with name RESET_PASSWORD_REQUEST")
-			} else {
-				passwordRecoveryEmailData := PasswordRecoveryEmailData{Id: identity.ID.String(), Email: address.Value, FirstName: firstName,
-					LastName: lastName, Name: firstName + " " + lastName, RecoveryURL: recoveryURL}
-				stringifyData, _ := json.Marshal(passwordRecoveryEmailData)
-				l.Info(string(stringifyData))
-				err = ch.Publish(
-					"",
-					"RESET_PASSWORD_REQUEST",
-					false,
-					false,
-					amqp.Publishing{
-						ContentType: "text/plain",
-						Body:        []byte(string(stringifyData)),
-					},
-				)
-
-				if err != nil {
-					l.Warn(err)
-				}
-				l.Info("Successfully Published Message to Queue")
-			}
-		}
-	}
+	RESET_PASSWORD_REQUEST
+	passwordRecoveryEmailData := PasswordRecoveryEmailData{Id: identity.ID.String(), Email: address.Value, FirstName: firstName,
+		LastName: lastName, Name: firstName + " " + lastName, RecoveryURL: recoveryURL}
+	stringifyData, _ := json.Marshal(passwordRecoveryEmailData)
+	l.Info(string(stringifyData))
+	// TODO: the routing name should be default and in ENV variables/config
+	SendMessageToQueue(rabbitMQURL, "RESET_PASSWORD_REQUEST", string(stringifyData), l)
 }
